a677ba6f8bac3391cba1c9cc8005089d045a7fa1
Nawab_Saab <furkanchisti786@gmail.com>
2024-12-02 01:00:01 +0530

    Expression vs Evaluated Expression

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,7 +1,25 @@
+import Coffee from "./coffee";
+
 function App() {
+  const username = "Talha Anjum";
+
   return (
     <>
-      <h1>Vite basic project</h1>
+      <Coffee />
+      <h1>Vite basic project!</h1>
+      <h3>Sigma Rapper {username} </h3>
+
+      {/* Expression vs Evaluated Expression
+      Expression: The code itself ({username}).
+
+      Evaluated Expression: The result of the code after execution ("Talha Anjum").
+
+      It's like writing a math problem:
+
+      Expression: 2 + 2.
+      Evaluated Expression: 4.  
+      
+      */}
     </>
   );
 }
