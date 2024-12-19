a12dcd7dbd8ed55affe5be391b0c019018f31ce4
Nawab_Saab <furkanchisti786@gmail.com>
2024-12-19 22:25:28 +0530

    custum files for  reusing in whole project

---
+++ 12_myblogwebapp/src/components/index.js
@@ -0,0 +1,14 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Login from "./Login";
+
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input, Login };
+
+//input color is changed due to forwardRef syntax
+
+//only for export in a single place that's it
